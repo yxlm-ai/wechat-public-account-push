1b5b1cf741535cdb339530601338e839f1eff299
yxlm-ai <85552513+yxlm-ai@users.noreply.github.com>
2022-08-30 16:53:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx8693324924f08a0f",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "1e4e498fa9dfe331312f00f48261f0d",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "da_N3EV93rY0aLpsWsQ6q5e1QUmjd08GXQDfhMGOi7o ",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["oczmp5sgOvPCoMOzEz8Pt9BSi_so ","oczmp5vtk3vREOS0IT0U0Wqb9m_I "],
      
     /**
      * 信息配置
@@ -19,9 +19,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "湖南",
     // 所在城市
-    CITY: "惠州",
+    CITY: "长沙",
 
     /** 重要节日相关 */
 
